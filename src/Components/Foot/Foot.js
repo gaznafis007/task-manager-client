8f0aa3653865c423de0426b43dfd94afd3e9f286
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2022-12-31 00:50:32 +0600

    footer

--- src/Components/Foot/Foot.js
+++ src/Components/Foot/Foot.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Foot = () => {
   return (
-    <div className="bg-light p-4">
-      <h2 className="">This is Footer</h2>
+    <div className="bg-light mx-auto p-4 position-fixed bottom-0 w-100">
+      <h2 className="ms-5">Task Manager</h2>
+      <p className="mt-2 ms-5">
+        &copy; All rights reserved to Task Manager til 2026
+      </p>
     </div>
   );
 };
